9bac4b459b1fb6ccae6f08a6741a27ebfab7178e
Blake Roddy <you@example.com>
2016-09-17 12:12:57 -0500

    linked fb database to js, html

---
+++ assets/javascript/beer-logic.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBCGNlsZyWEj1KbRpXx6bmUM1T_sVO9_3M",
+    authDomain: "keep-austin-beered.firebaseapp.com",
+    databaseURL: "https://keep-austin-beered.firebaseio.com",
+    storageBucket: "keep-austin-beered.appspot.com",
+    messagingSenderId: "57492481901"
+  };
+  firebase.initializeApp(config);
