7a4e9f09a578c0738b4c2416370200b8de332cf1
Mical Juarez <mical.juarez@gmail.com>
2016-09-24 13:32:12 -0500

    tried yelp api stuff, problem using oauth

--- assets/javascript/yelp-api.js
+++ assets/javascript/yelp-api.js
@@ -1,15 +1,52 @@
 
-function getBusinessInfo() {
+var auth = {
+        consumerKey : "eyg4gO8DoW0MTxS-acg1mQ",
+        consumerSecret : "vrd1nqrlH9FhfcGMIQukf9aDgKw",
+        accessToken : "eAmVEHEByNF8s6W38OAPOzbNmA-BhYgb",
+        accessTokenSecret : "GxcGN8OA-mL-xSI6nBftfWM_Yc0",
+        serviceProvider : {
+          signatureMethod : "HMAC-SHA1"
+        }
+};
 
-  var type = $(this).attr("data-name");
+var terms = 'food';
+var near = 'Austin';
 
-  var queryURL = 
+var accessor = {
+  consumerSecret : auth.consumerSecret,
+  tokenSecret : auth.accessTokenSecret
+};
+
+parameters = [];
+parameters.push(['term', terms]);
+parameters.push(['location', near]);
+parameters.push(['callback', 'cb']);
+parameters.push(['oauth_consumer_key', auth.consumerKey]);
+parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+parameters.push(['oauth_token', auth.accessToken]);
+parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+var message = {
+        'action' : 'http://api.yelp.com/v2/search',
+        'method' : 'GET',
+        'parameters' : parameters
+};
+
+OAuth.setTimestampAndNonce(message);
+OAuth.SignatureMethod.sign(message, accessor);
 
 
+var parameterMap = OAuth.getParameterMap(message.parameters);
+parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+console.log(parameterMap);
 
-
-
-
-
-
-}
+$.ajax({
+        'url' : message.action,
+        'data' : parameterMap,
+        'cache' : true,
+        'dataType' : 'jsonp',
+        'jsonpCallback' : 'cb',
+        'success' : function(data, textStats, XMLHttpRequest) {
+          console.log(data);
+        }
+});
